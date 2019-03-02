6c5b7391d16307aa2c3ef0289c08bacdc8c1d3a3
maximechofflet <45247673+maximechofflet@users.noreply.github.com>
2019-03-02 13:14:32 +0100

    Version 02/2019

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 2000);
+	});
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
